f2d22e2521d99eb2994ded04cdf2078e7b4da3ad
Luke Godfrey <thelukester92@me.com>
2012-05-14 21:46:03 -0500

    first commit!!!

---
+++ distracted-driving/distracted-driving/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  distracted-driving
+//
+//  Created by Luke Godfrey on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ distracted-driving/distracted-drivingTests/distracted_drivingTests.h
@@ -0,0 +1,13 @@
+//
+//  distracted_drivingTests.h
+//  distracted-drivingTests
+//
+//  Created by Luke Godfrey on 5/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface distracted_drivingTests : SenTestCase
+
+@end
