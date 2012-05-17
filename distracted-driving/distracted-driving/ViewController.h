f898d541e2a07cbfb989d5d12fe6fa03b753e099
Luke Godfrey <thelukester92@me.com>
2012-05-17 01:39:55 -0500

    added sql with dummy row entries

--- distracted-driving/distracted-driving/ViewController.h
+++ distracted-driving/distracted-driving/ViewController.h
@@ -7,7 +7,30 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <sqlite3.h>
 
 @interface ViewController : UIViewController
+{
+	UIButton	*startButton;
+	UILabel		*recordingLabel;
+	UILabel		*dataLabel;
+	sqlite3		*db;
+	NSTimer		*ticker;
+	BOOL		dataExists;
+	const char	*dbpath;
+}
+
+@property (nonatomic, retain) IBOutlet UIButton	*startButton;
+@property (nonatomic, retain) IBOutlet UILabel	*recordingLabel;
+@property (nonatomic, retain) IBOutlet UILabel	*dataLabel;
+@property (nonatomic, retain) NSTimer			*ticker;
+@property (nonatomic, assign) const char		*dbpath;
+@property (nonatomic, assign) BOOL				dataExists;
+
+- (void)setDataExists:(BOOL)value;
+- (BOOL)sqlcon;
+- (BOOL)insertRowWithAccelorometer:(NSString *)accelorometer andSound:(NSString *)sound andGps:(NSString *)gps andCompass:(NSString *)compass andBattery:(NSString *)battery;
+- (void)record:(id) sender;
+- (IBAction)toggleButton:(id) sender;
 
 @end
--- distracted-driving/distracted-driving/ViewController.m
+++ distracted-driving/distracted-driving/ViewController.m
@@ -10,6 +10,147 @@
 
 @implementation ViewController
 
+@synthesize startButton, recordingLabel, dataLabel, dbpath, ticker, dataExists;
+
+
+- (void)setDataExists:(BOOL)value
+{
+	dataExists = value;
+	
+	if(dataExists)
+		[dataLabel setText:@"You have recorded data to send."];
+	else
+		[dataLabel setText:@"You do not have recorded data to send."];
+}
+
+// Connect to the SQL database
+- (BOOL)sqlcon
+{
+	NSString		*database;
+	NSArray			*paths;
+	
+	paths		= NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	database	= [[NSString alloc] initWithString:[[paths objectAtIndex:0] stringByAppendingPathComponent:@"distracted-driving.db"]];
+	
+	if([[NSFileManager defaultManager] fileExistsAtPath:database] == NO)
+	{
+		NSString *dbpathFromApp = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"distracted-driving.db"];
+		[[NSFileManager defaultManager] copyItemAtPath:dbpathFromApp toPath:database error:nil];
+		
+		NSLog(@"that way");
+		
+		dbpath = [database UTF8String];
+		
+		if(sqlite3_open(dbpath, &db) == SQLITE_OK)
+		{
+			char *err;
+			const char *sql = "CREATE TABLE IF NOT EXISTS collected_data (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATETIME, accelorometer TEXT, sound TEXT, gps TEXT, compass TEXT, battery TEXT)";
+			
+			if(sqlite3_exec(db, sql, NULL, NULL, &err) != SQLITE_OK)
+			{
+				NSLog(@"Table creation failed!");
+				sqlite3_close(db);
+				return NO;
+			}
+			
+			sqlite3_close(db);
+		}
+		else
+		{
+			NSLog(@"Database connection failed!");
+			return NO;
+		}
+	}
+	else
+	{
+		dbpath = [database UTF8String];
+		
+		if(sqlite3_open(dbpath, &db) != SQLITE_OK)
+		{
+			NSLog(@"Database connection failed!");
+			sqlite3_close(db);
+			return NO;
+		}
+		
+		sqlite3_close(db);
+	}
+	
+	return YES;
+}
+
+// Insert a row to SQL
+- (BOOL)insertRowWithAccelorometer:(NSString *)accelorometer andSound:(NSString *)sound andGps:(NSString *)gps andCompass:(NSString *)compass andBattery:(NSString *)battery
+{
+	sqlite3_stmt	*query;
+	
+	if(sqlite3_open(dbpath, &db) == SQLITE_OK)
+	{
+		NSString *nsquery = [NSString stringWithFormat:@"INSERT INTO collected_data (date, accelorometer, sound, gps, compass, battery) VALUES (datetime('now'), '%@', '%@', '%@', '%@', '%@')", accelorometer, sound, gps, compass, battery];
+		
+		const char *cquery = [nsquery UTF8String];
+		
+		sqlite3_prepare_v2(db, cquery, -1, &query, NULL);
+		
+		if(sqlite3_step(query) != SQLITE_DONE)
+		{
+			NSLog(@"Query failed (%s)!", sqlite3_errmsg(db));
+			sqlite3_finalize(query);
+			sqlite3_close(db);
+			return NO;
+		}
+		
+		sqlite3_finalize(query);
+		sqlite3_close(db);
+	}
+	else
+		NSLog(@"Query failed (%s)!", sqlite3_errmsg(db));
+	
+	return YES;
+}
+
+- (void)record:(id)sender
+{
+	// For each tick, add a row to SQL	
+	NSString *accelorometer		= @"Unknown";
+	NSString *sound				= @"Unknown";
+	NSString *gps				= @"Unknown";
+	NSString *compass			= @"Unknown";
+	NSString *battery			= [NSString stringWithFormat:@"%f", [[UIDevice currentDevice] batteryLevel]];
+	
+	[self insertRowWithAccelorometer:accelorometer andSound:sound andGps:gps andCompass:compass andBattery:battery];
+}
+
+- (IBAction)toggleButton:(id)sender
+{
+	startButton.selected = !startButton.selected;
+	
+	if(startButton.selected)
+	{
+		// Recording
+		
+		// Since we're recording, data will need to be sent
+		[self setDataExists:YES];
+		
+		// Since we started recording, tell the user we're recording
+		[recordingLabel setText:@"You are recording."];
+		
+		// Start recording
+		ticker = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(record:) userInfo:nil repeats:YES];
+	}
+	else
+	{
+		// Done recording
+		
+		// Tell the user we've stopped recording
+		[recordingLabel setText:@"You are not recording."];
+		
+		// Stop recording
+		[ticker invalidate];
+		ticker = nil;
+	}
+}
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -21,14 +162,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	// Set button labels
+	[startButton setTitle:@"Start Recording"	forState:UIControlStateNormal];
+	[startButton setTitle:@"Stop Recording"		forState:UIControlStateSelected];
+	
+	// Set initial label text
+	[recordingLabel	setText:@"You are not recording."];
+	[dataLabel		setText:@"You do not have recorded data to send."];
+	
+	// Set initial booleans
+	[self setDataExists:NO];
+	
+	// Connect to SQL
+	[self sqlcon];
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+    
+	self.startButton	= nil;
+	self.recordingLabel	= nil;
+	self.dataLabel		= nil;
+	self.ticker			= nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
